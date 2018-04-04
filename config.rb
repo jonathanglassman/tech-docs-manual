edbf935bcf2c7709991bbd43b983908d5f4b1df8
Tijmen Brommet <tijmen@gmail.com>
2018-04-04 14:34:28 +0100

    Create MVP documentation
    This creates the first version of the documentation for the
    tech-docs-template.
    
    The content comes from the various repositories:
    
    - https://github.com/alphagov/tech-docs-template README &
    CONTRIBUTING.md
    - https://github.com/alphagov/tech-docs-gem README
    
    We also pull in the options for `config/tech-docs.yml` from the /docs
    folder in https://github.com/alphagov/tech-docs-gem.

--- config.rb
+++ config.rb
@@ -1,3 +1,18 @@
 require 'govuk_tech_docs'
 
 GovukTechDocs.configure(self)
+
+helpers do
+  def template_configuration_options
+    gem_location = Bundler.rubygems.find_name('govuk_tech_docs').first.full_gem_path
+    raw_markdown = File.read(gem_location + "/docs/configuration.md")
+
+    # Strip the h1 header
+    markdown = raw_markdown.lines[1..-1].join
+
+    # Convert the h2 headers to h3
+    markdown.gsub!('##', '####')
+
+    markdown
+  end
+end
