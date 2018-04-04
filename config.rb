748659ae276e49792d13a2ec60e03297cd087a21
Tijmen Brommet <tijmen@gmail.com>
2018-04-04 14:42:51 +0100

    Use relative assets to allow publishing on GH pages

--- config.rb
+++ config.rb
@@ -2,6 +2,9 @@ require 'govuk_tech_docs'
 
 GovukTechDocs.configure(self)
 
+activate :relative_assets
+set :relative_links, true
+
 helpers do
   def template_configuration_options
     gem_location = Bundler.rubygems.find_name('govuk_tech_docs').first.full_gem_path
