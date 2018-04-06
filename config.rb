4363869c934d01e620f44401bb713b82093b52e5
Tijmen Brommet <tijmen@gmail.com>
2018-04-06 14:06:18 +0100

    Import images from gem

--- config.rb
+++ config.rb
@@ -7,6 +7,8 @@ set :relative_links, true
 
 DOCS_LOCATION_IN_GEM = Bundler.rubygems.find_name('govuk_tech_docs').first.full_gem_path + "/docs"
 
+files.watch :source, path: DOCS_LOCATION_IN_GEM
+
 helpers do
   def gem_docs(filename)
     raw_markdown = File.read(DOCS_LOCATION_IN_GEM + "/#{filename}")
